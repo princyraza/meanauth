1e40ff7f13c67c952cb63f967151882d0715e61d
Princy Razafimanantsoa <princy.raza@gmail.com>
2017-05-03 17:15:03 +0200

    refactor comments

--- app.js
+++ app.js
@@ -6,35 +6,36 @@ const passport = require('passport')
 const mongoose = require('mongoose')
 const config = require('./config/database')
 
-// Connect to database
+// Connect To Database
 mongoose.connect(config.database)
 
-// On connection
+// On Connection
 mongoose.connection.on('connected', () => {
   console.log('Connected to database ' + config.database)
 })
 
-// On error
+// On Error
 mongoose.connection.on('error', (err) => {
-  console.log('Database error ' + err)
+  console.log('Database error: ' + err)
 })
 
 const app = express()
 
 const users = require('./routes/users')
 
+// Port Number
 const port = 3000
 
-// CORS middleware
+// CORS Middleware
 app.use(cors())
 
-// Set Static folder
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Body-parser middleware
+// Body Parser Middleware
 app.use(bodyParser.json())
 
-// Passport middleware
+// Passport Middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -42,11 +43,12 @@ require('./config/passport')(passport)
 
 app.use('/users', users)
 
-// index route
+// Index Route
 app.get('/', (req, res) => {
-  res.send('Invalid endpoint')
+  res.send('Invalid Endpoint')
 })
 
+// Start Server
 app.listen(port, () => {
   console.log('Server started on port ' + port)
 })
